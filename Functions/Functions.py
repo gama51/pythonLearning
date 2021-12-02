08d827f23db5dc3eaabc3df416458662a5a2d1ea
Gabriel <gama51dr@gmail.com>
2021-12-02 13:55:43 -0500

    feat: The functios feature was created

---
+++ Functions/Functions.py
@@ -0,0 +1,26 @@
+# def print_message():
+#     print("Special message:")
+#     print("¡I'm learning to use functions with python")
+
+# print_message()
+
+def conversation(message):
+   print('Hello')
+   print('How are you?')
+   print('You have selected option '+message)
+   print('Good bye')
+
+options=input('please select an option (1,2,3): ')
+if options=='1':
+   conversation(options)
+elif options=='2':
+    conversation(options)
+elif options=='3':
+    conversation(options)
+else:
+    print('Please type a rigth option')
+
+
+
+
+
