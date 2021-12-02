dd98cd0fa1cf35655c7ed7ec100fad30ea647d36
Gabriel <gama51dr@gmail.com>
2021-12-02 12:42:47 -0500

    feat: se desarrolla el ejercicio basico de condicinoales

---
+++ Conditionals/conditionals.py
@@ -0,0 +1,14 @@
+# edad= int(input("How old are you?: "))
+# if edad > 17:
+#     print("You are of legal age")
+# else:
+#     print("You are a minor")
+
+numero=int(input("Type a number: "))
+
+if numero >5 :
+    print("Number is greater than 5")
+elif numero ==5:
+    print("Number is equal 5")
+else:
+    print("Number is less than 5")
