455391c2d199370c45a6c780ec45c943b9b0d734
Gabriel <gama51dr@gmail.com>
2021-12-02 11:21:10 -0500

    feat: se redondea a dos decimales

--- convertionCoin/conversor.py
+++ convertionCoin/conversor.py
@@ -1,7 +1,7 @@
 pesos = input("¿Cúantos pesos Colombianos tienes?: ")
 pesos = float(pesos)
 valor_dolar=3875
-dolares=pesos/valor_dolar
+dolares=round(pesos/valor_dolar,2)
 dolares=str(dolares)
 print("Tienes $ "+dolares+" Dolares")
 
