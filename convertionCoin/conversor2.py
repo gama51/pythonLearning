887e336a4b0ce5d08ab1497b0c7fdae689249491
Gabriel <gama51dr@gmail.com>
2021-12-02 11:31:24 -0500

    feat: se módifica el valor del dolar

--- convertionCoin/conversor2.py
+++ convertionCoin/conversor2.py
@@ -1,6 +1,6 @@
 dolares = input("¿Cúantos USD tienes?: ")
 dolares = float(dolares)
-valor_dolar=3875
+valor_dolar=3946.90
 pesos=round(valor_dolar*dolares,2)
 pesos=str(pesos)
 print("Tienes $ "+pesos+" COP")
